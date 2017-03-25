1bad5b875f7b5451410a1336c35797b1cff0ba9a
r.courivaud@gmail.com <Gi8wXaijei!>
2017-03-25 23:04:59 +0100

    add authentification to mongo

--- pandas_mongodb/__init__.py
+++ pandas_mongodb/__init__.py
@@ -18,11 +18,15 @@ class PandasMongoDB:
         return pd.DataFrame(list(self.client[db][col].find(find_query)))
 
     def insert_dataframe_into_collection(self, db, col, dataframe):
+        if self.usr and self.pwd:
+            self.client[db].authenticate(self.usr, self.pwd, mechanism='SCRAM-SHA-1')
         self.client[db][col].insert_many(dataframe.to_dict('records'))
 
     def delete_element_from_collection(self, db, col, find_query=None):
         if find_query is None:
             find_query = {}
+        if self.usr and self.pwd:
+            self.client[db].authenticate(self.usr, self.pwd, mechanism='SCRAM-SHA-1')
         result = self.client[db][col].delete_many(find_query)
         print("Remove {} element in {}.{}".format(str(result.deleted_count), db, col))
         return result
